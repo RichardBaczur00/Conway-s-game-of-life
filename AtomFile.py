ccac57325db6cdec37aa8a7aa8c8080643703666
Richard Baczur <richardbaczur1@gmail.com>
2020-02-22 21:22:03 +0200

    Added a simulator, still needs some tweaks, but the project is nearly done.

--- AtomFile.py
+++ AtomFile.py
@@ -1,32 +1,62 @@
 class Coordinate:
+    #Built this structure only to make the code more readable
     def __init__(self, x, y):
         self.x = x;
         self.y = y;
 
 
 class Atom:
+    #This structure represents a molecule
     def __init__(self, start_coordinate):
         self.coordinate = start_coordinate
         self.is_alive = False
 
-    
+    '''
+        Function used the determine if a molecule will survive or not
+        @param: self is the molecule that is being tested
+        @param: grid is the collections of molecules that compose the grid(both dead and alive)
+        @return: returns True if the molecule will survive the iteration or if it dies
+        The rules are easy to deduce from the code or they can be found on the internet
+    '''
     def is_surviving(self, grid):
         neighbours = self.get_neighbours(grid)
         if len(neighbours) < 2:
             return False
         elif len(neighbours) > 3:
             return False
-        elif len(neighbours) == 3:
-            this.reproduce(grid)
-            return True
         else:
             return True
-    
 
-    def revive(new_x, new_y):
-        grid[new_x][new_y].is_alive = True
-    
 
+    '''
+        Function determines if a set of coordinates belong to the grid
+        @param: x is the first coordinate(x-axis)
+        @param: y is the second coordinate(y-axis)
+        @param: grid is the collection of molecules that compose the grid(both dead and alive)
+        @return: <type: bool> True if the coordinates belong to the grid, False otherwise
+    '''
+    def test_coordinates(self, x, y, grid):
+        if x >= len(grid) - 1 or x < 0:
+            return False
+        elif y >= len(grid[0]) - 1 or y < 0:
+            return False
+        return True
+
+    
+    '''
+        Function used to revive a dead molecule
+        @param: self is the molecule that will be removed
+        @return: void
+    '''
+    def revive(self):
+        self.is_alive = True
+    
+    '''
+        Function returns the living molecules that surround the molecule sent as parameter
+        @param: self is the molecule in question
+        @param: grid is the collection of molecules that compose the grid(both dead and aive)
+        @return: <type: list> a list of all the living molecules surrounding the molecule referenced
+    '''
     def get_neighbours(self, grid):
         ret = []
 
@@ -35,17 +65,26 @@ class Atom:
 
         tx = [0, 0, -1, 1, -1, -1, 1, 1]
         ty = [-1, 1, 0, 0, -1, 1, -1, 1]
-
         for i in range(len(tx)):
-           new_x = initial_x + tx[i]
-           new_y = initial_y + ty[i]
+            new_x = initial_x + tx[i]
+            new_y = initial_y + ty[i]
 
-           if grid[new_x][new_y].is_alive:
-                ret.append(grid[new_x][new_y])
+            if not self.test_coordinates(new_x, new_y, grid):
+                continue
+            try:
+                if grid[new_x][new_y].is_alive:
+                    ret.append(grid[new_x][new_y])
+            except IndexError:
+                print("{{ Checking molecule x: {}; y: {}; }}".format(new_x, new_y))
 
         return ret
 
-    
+    '''
+        Function that determines if a molecule will be revived
+        @param: self is the molecule in question
+        @param: grid is the collection of molecules that compose the grid(both dead and alive)
+        @return: void
+    '''
     def reproduce(self, grid):
         initial_x = self.coordinate.x
         initial_y = self.coordinate.y
@@ -57,7 +96,10 @@ class Atom:
             new_x = initial_x + tx[i]
             new_y = initial_y + ty[i]
 
+            if not self.test_coordinates(new_x, new_y, grid):
+                continue
+
             number_of_neighbours = len(grid[new_x][new_y].get_neighbours(grid))
             
             if number_of_neighbours == 3:
-                revive(new_x, new_y)
+                self.revive()
--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 from AtomFile import Atom, Coordinate
+import os
+from time import sleep
 
 n = int(input("Please enter the number of lines for the grid: "))
 m = int(input("Please enter the number of columns for the grid: "))
 
+n += 1
+m += 1
 grid = []
 grid.append([])
 for i in range(n):
     for j in range(m):
         coordinate = Coordinate(i, j)
+        print("{{ Added molecule x: {}; y: {} }}".format(i, j))
         atom = Atom(coordinate)
         grid[i].append(atom)
     grid.append([])
 
-for line in grid:
-    for atom in line:
-        print(atom, end = ' ')
-    print()
+grid[1][2].revive()
+grid[0][2].revive()
+grid[1][1].revive()
+grid[2][1].revive()
 
+def print_grid():
+    for line in grid:
+        for atom in line:
+            if atom.is_alive:
+                print("#", end='')
+            else:
+                print("0", end='')
+        print()
+    return None
+
+
+def simulate():
+    deaths = []
+    
+    for line in grid:
+        for molecule in line:
+            if molecule.is_alive and not molecule.is_surviving(grid):
+                deaths.append(molecule)
+            elif not molecule.is_alive:
+                molecule.reproduce(grid)
+
+    for dead_molecule in deaths:
+        dead_molecule.is_alive = False
+
+    print_grid()
+
+
+for i in range(1, 10000000):
+    simulate()
+    sleep(1)
+    os.system('clear')
