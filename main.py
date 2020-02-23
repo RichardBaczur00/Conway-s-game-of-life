95da334db278e3ec4b9b6717802912bb5153bb06
Richard Baczur <richardbaczur1@gmail.com>
2020-02-23 17:28:24 +0200

    The algorithm works well, at least on the tests I implemented yet.

--- main.py
+++ main.py
@@ -17,39 +17,53 @@ for i in range(n):
         grid[i].append(atom)
     grid.append([])
 
-grid[1][2].revive()
-grid[0][2].revive()
-grid[1][1].revive()
-grid[2][1].revive()
 
 def print_grid():
     for line in grid:
         for atom in line:
             if atom.is_alive:
-                print("#", end='')
+                print("1", end='')
             else:
                 print("0", end='')
         print()
     return None
 
 
+def build_initial_celular_automata():
+    number_of_live_molecules = int(input("Please enter the number of live cells you wish to start with: "))
+    
+    for i in range(number_of_live_molecules):
+        x_coordinate = int(input("{{ Please enter the x coordiante of the {}-(th) molecule: }}".format(i)))
+        y_coordinate = int(input("{{ Please enter the y coordiante of the {}-(th) molecule: }}".format(i))) 
+
+        grid[x_coordinate][y_coordinate].revive()
+
+    print_grid()
+
+
 def simulate():
     deaths = []
+    revives = []
     
     for line in grid:
         for molecule in line:
             if molecule.is_alive and not molecule.is_surviving(grid):
                 deaths.append(molecule)
             elif not molecule.is_alive:
-                molecule.reproduce(grid)
-
+                if len(molecule.get_neighbours(grid)) == 3:
+                    revives.append(molecule)
+    
+    for revived_molecule in revives:
+        revived_molecule.revive()
+    
     for dead_molecule in deaths:
         dead_molecule.is_alive = False
-
+    
     print_grid()
 
 
+build_initial_celular_automata()
 for i in range(1, 10000000):
     simulate()
-    sleep(1)
+    sleep(0.5)
     os.system('clear')
